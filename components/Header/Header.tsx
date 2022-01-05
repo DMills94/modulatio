931947393fce999e52d86cedc996bb09d9e2b843
DMills94 <davidj-mills@live.co.uk>
2022-01-05 16:34:44 +0000

    chore: reformat file

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -10,10 +10,18 @@ export default function Header() {
     <header className={styles.header}>
       <h1>Modulat.io</h1>
       <div className={styles.icons}>
-        <a href="https://twitter.com/modulat_io" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className={styles.icon} icon={faTwitter} /></a>
-        <a href="https://discord.com/invite/v7CeBe34Qu" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className={styles.icon} icon={faDiscord} /></a>
-        <a href="https://open.spotify.com/show/4SakicRp0mPXVyEcFtXio1" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className={styles.icon} icon={faSpotify} /></a>
-        <a href="https://linktr.ee/modulat.io" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className={styles.icon} icon={faFileAlt} /></a>
+        <a href="https://twitter.com/modulat_io" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className={styles.icon} icon={faTwitter} />
+        </a>
+        <a href="https://discord.com/invite/v7CeBe34Qu" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className={styles.icon} icon={faDiscord} />
+        </a>
+        <a href="https://open.spotify.com/show/4SakicRp0mPXVyEcFtXio1" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className={styles.icon} icon={faSpotify} />
+        </a>
+        <a href="https://linktr.ee/modulat.io" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon className={styles.icon} icon={faFileAlt} />
+        </a>
       </div>
     </header>
   );
